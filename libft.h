0d34805782cb024c3a5c1174bed4ea4fd3ab7ece
Isel Villoldo Guerrero <ivillold@c2r19s3.42madrid.com>
2024-10-07 22:02:48 +0200

    v0.1 7/34

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivillold <ivillold@student.your42netw      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 21:02:33 by ivillold          #+#    #+#             */
+/*   Updated: 2024/10/07 21:08:13 by ivillold         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+//LIBS//
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+#endif
